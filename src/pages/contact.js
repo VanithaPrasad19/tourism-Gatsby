3c321d722b0dab1017e2c581d36c860894f5176a
VanithaPrasad19 <vanithavsvanitha@gmail.com>
2020-06-12 16:12:38 +0530

    contacts

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,27 +2,29 @@ import React from 'react'
 import Layout from "../components/Layout"
 import StyledHero from "../components/StyledHero"
 import { graphql } from "gatsby"
+import Contact from "../components/Contact"
 
 export const query = graphql`
 query {
-    contactBcg: file(relativePath: {eq: "connectBcg.jpeg"}) {
-        childImageSharp {
-            fluid(quality: 90, maxWidth: 4160) {
-                ...GatsbyImageSharpFluid_withWebp
-            }
-        }
-    }
+contactBcg: file(relativePath: {eq: "connectBcg.jpeg"}) {
+childImageSharp {
+fluid(quality: 90, maxWidth: 4160) {
+...GatsbyImageSharpFluid_withWebp
+}
+}
+}
 }
 `;
 
 const contact = ({ data }) => {
-    return (
-        <Layout>
-            <StyledHero img={data.contactBcg.childImageSharp.fluid}>
-                Contact page
-            </StyledHero>
-        </Layout>
-    )
+return (
+<Layout>
+<StyledHero img={data.contactBcg.childImageSharp.fluid}>
+Contact page
+</StyledHero>
+<Contact />
+</Layout>
+)
 }
 
 export default contact
