ab0b04d2de7d946558f4ff72f563172af74612a1
VanithaPrasad19 <vanithavsvanitha@gmail.com>
2020-06-13 17:52:04 +0530

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,25 @@
-module.exports = {
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+  })
+  
+  module.exports = {
   plugins: [
   {
   resolve: `gatsby-source-filesystem`,
   options: {
-  name: `images`,
-  path: `${__dirname}/src/images/`,
+    name: `images`,
+    path: `${__dirname}/src/images/`,
+    },
   },
+  {
+  resolve: `gatsby-source-contentful`,
+  options: {
+    spaceId: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
   },
-  `gatsby-plugin-styled-components`,
-  `gatsby-transformer-sharp`,
-  `gatsby-plugin-sharp`
+    `gatsby-plugin-styled-components`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`
   ],
   }
